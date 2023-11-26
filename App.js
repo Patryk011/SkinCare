6d602435704e049346e3405f136e298793f25f09
Patryk <patryknartowski@wp.pl>
2023-11-26 16:39:10 +0100

    small fix

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
-import { registerRootComponent } from "expo";
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AppNavigation } from "./navigation/AppNavigaton";
 
-class App extends React.Component {
-  render() {
-    return (
-      <NavigationContainer>
-        <AppNavigation />
-      </NavigationContainer>
-    );
-  }
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigation />
+    </NavigationContainer>
+  );
 }
-
-registerRootComponent(App);
